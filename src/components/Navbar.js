f679cb85ba0e025470f064121a7410e0b7863136
willis plummer <willisplummer@gmail.com>
2018-05-28 17:15:55 -0400

    rework nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,31 +7,33 @@ import logo from '../img/logo.svg'
 const Navbar = () => (
   <nav className="navbar is-transparent">
     <div className="container">
-      <div className="navbar-brand">
-        <Link to="/" className="navbar-item">
-          <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-          </figure>
-        </Link>
-      </div>
       <div className="navbar-start">
+        <span className="navbar-item">
+          May Issue
+        </span>
+      </div>
+      <div className="navbar-end">
         <Link className="navbar-item" to="/about">
           About
         </Link>
-        <Link className="navbar-item" to="/products">
-          Products
+        <Link className="navbar-item" to="/about">
+          Archive
         </Link>
-      </div>
-      <div className="navbar-end">
         <a
           className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
+          href="http://www.newestyork.co/buy-a-book/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
+          Shop
+        </a>
+        <a
+          className="navbar-item"
+          href="http://www.newestyork.co/blog/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Etc
         </a>
       </div>
     </div>
