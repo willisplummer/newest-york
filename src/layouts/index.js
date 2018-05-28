285e3f374236acbe0be156778513b50723840328
willis plummer <willisplummer@gmail.com>
2018-05-28 19:17:36 -0400

    logo placeholder

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-
 import Navbar from '../components/Navbar'
+import Logo from '../components/Logo'
 import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet title="Newest York" />
+    <Logo color="red"/>
     <Navbar />
     <div>{children()}</div>
   </div>
