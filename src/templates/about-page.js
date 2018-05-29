ca0f45d797c391f2febdd4edd3922204392c5908
Willis Plummer <willisplummer@gmail.com>
2018-05-29 00:54:46 -0400

    about page

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -1,35 +1,60 @@
 import React from 'react'
+import {markdown} from 'markdown'
 import Content, { HTMLContent } from '../components/Content'
+import Navbar from '../components/Navbar'
+import _ from 'lodash'
 
-export const AboutPageTemplate = ({ title, content, contentComponent }) => {
+export const AboutPageTemplate = ({ title, content, contentComponent, masthead, callForSubmissions, issueMonthYear }) => {
   const PageContent = contentComponent || Content
 
   return (
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                {title}
-              </h2>
-              <PageContent className="content" content={content} />
+    <div>
+      <Navbar issueMonthYear={issueMonthYear} />
+      <section className="section section--gradient">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-6">
+              <div className="section">
+                <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+                  About
+                </h2>
+                <PageContent className="content" content={content} />
+                <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+                  Submitting
+                </h2>
+                <PageContent className="content" content={callForSubmissions} />
+              </div>
+            </div>
+            <div className="column is-6">
+              <div className="section">
+                <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+                  Masthead
+                </h2>
+                <PageContent className="content" content={masthead} />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   )
 }
 
 const AboutPage = ({ data }) => {
-  const { markdownRemark: post } = data
+  const { markdownRemark: post, allMarkdownRemark } = data
+  const callForSubmissions = markdown.toHTML(post.frontmatter.callForSubmissions)
+  const masthead = markdown.toHTML(post.frontmatter.masthead)
 
+  const latestIssueMonthYear = _.get(allMarkdownRemark, 'edges[0].node.frontmatter.issueMonthYear')
+  console.log(allMarkdownRemark.edges[0])
   return (
     <AboutPageTemplate
       contentComponent={HTMLContent}
       title={post.frontmatter.title}
+      masthead={masthead}
+      callForSubmissions={callForSubmissions}
       content={post.html}
+      issueMonthYear={latestIssueMonthYear}
     />
   )
 }
@@ -42,6 +67,21 @@ export const aboutPageQuery = graphql`
       html
       frontmatter {
         title
+        masthead
+        callForSubmissions
+      }
+    }
+    allMarkdownRemark(
+      limit: 1,
+      sort: { order: DESC, fields: [frontmatter___date] },
+      filter: { frontmatter: { templateKey: { eq: "issue" } }}
+    ) {
+      edges {
+        node{
+          frontmatter {
+            issueMonthYear: date(formatString: "MMMM YYYY")
+          }
+        }
       }
     }
   }
