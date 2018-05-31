c74c137f963cb9efd1e595054460d09bcf216017
willis plummer <willisplummer@gmail.com>
2018-05-31 01:23:48 -0400

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+'use strict';
+
+module.exports = {
+  parser: 'babel-eslint',
+  extends: ['airbnb', 'prettier'],
+  plugins: ['prettier', 'import', 'flowtype', 'prefer-object-spread'],
+  env: {
+    browser: true,
+    node: true,
+    jest: true
+  },
+  globals: {
+    mount: false,
+    render: false,
+    shallow: false
+  },
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: './config/webpack.config.base.js'
+      }
+    }
+  },
+  rules: {
+    'no-unused-vars': 2,
+    'import/no-anonymous-default-export': 2,
+    'prefer-object-spread/prefer-object-spread': 2,
+    'flowtype/define-flow-type': 1,
+    'react/jsx-filename-extension': 0,
+    'react/prop-types': 0,
+    'react/require-default-props': 0,
+    'import/no-extraneous-dependencies': ['error', {
+      devDependencies: true
+    }],
+    'jsx-a11y/anchor-is-valid': ['error', {
+      components: ['Link'],
+      specialLink: ['to', 'hrefLeft', 'hrefRight'],
+      aspects: ['noHref', 'invalidHref', 'preferButton']
+    }],
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'all'
+      }
+    ]
+  }
+}
