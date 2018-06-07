7cd5014ab464055611d3e816a1127d6d676891f6
willis plummer <willisplummer@gmail.com>
2018-06-06 23:31:50 -0400

    refactored

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,54 +1,45 @@
 import React from 'react';
-import _ from 'lodash';
+import sortBy from 'lodash/sortBy';
 import Article from '../components/current-issue/Article';
 import IssueTitle from '../components/current-issue/IssueTitle';
 import Layout from '../components/shared/Layout';
 
 const IndexPage = ({ data }) => {
-  const { edges: posts } = data.allMarkdownRemark;
-  const issues = posts.filter(
-    post => post.node.frontmatter.templateKey === 'issue',
-  );
+  const { edges: issues } = data.allMarkdownRemark;
 
   const latestIssue = issues[0].node;
-  const latestIssueNumber = issues.length;
-  const latestIssueMonth = latestIssue.frontmatter.date.split(' ')[0];
-  const latestIssueYear = latestIssue.frontmatter.date.split(' ')[2];
-  const issueMonthYear = `${latestIssueMonth} ${latestIssueYear}`;
+  const latestIssueNumber = issues.totalCount;
+
   const {
     textColor,
     backgroundColor,
     title: latestIssueTitle,
+    publicationMonthYear,
   } = latestIssue.frontmatter;
 
-  const articles = posts.filter(
-    post => post.node.frontmatter.templateKey === 'article',
-  );
+  const { fields: { articles } } = latestIssue;
 
-  const latestIssueArticles = _.sortBy(
-    articles.filter(
-      article => article.node.frontmatter.issue === latestIssueTitle,
-    ),
-    article => article.node.frontmatter.order,
-  );
+  const sortedArticles = sortBy(articles, 'frontmatter.order');
 
   return (
     <Layout
-      issueMonthYear={issueMonthYear}
+      issueMonthYear={publicationMonthYear}
       textColor={textColor}
       backgroundColor={backgroundColor}
     >
       <section className="section">
         <IssueTitle title={latestIssueTitle} issueNumber={latestIssueNumber} />
-        {latestIssueArticles.map(({ node: post }) => (
-          <Article
-            key={post.fields.slug}
-            slug={post.fields.slug}
-            title={post.frontmatter.title}
-            author={post.frontmatter.author}
-            subtitle={post.frontmatter.subtitle}
-          />
-        ))}
+        {sortedArticles.map(
+          ({ fields: { slug }, frontmatter: { title, author, subtitle } }) => (
+            <Article
+              key={slug}
+              slug={slug}
+              title={title}
+              author={author}
+              subtitle={subtitle}
+            />
+          ),
+        )}
       </section>
     </Layout>
   );
@@ -59,22 +50,32 @@ export default IndexPage;
 // eslint-disable-next-line no-undef
 export const pageQuery = graphql`
   query IndexQuery {
-    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+    allMarkdownRemark(
+      limit: 1
+      sort: { order: DESC, fields: [frontmatter___date] }
+      filter: { frontmatter: { templateKey: { eq: "issue" } } }
+    ) {
+      totalCount
       edges {
         node {
-          excerpt(pruneLength: 400)
           id
           fields {
             slug
+            articles {
+              fields {
+                slug
+              }
+              frontmatter {
+                author
+                order
+                title
+                subtitle
+              }
+            }
           }
           frontmatter {
             title
-            subtitle
-            templateKey
-            date(formatString: "MMMM DD, YYYY")
-            issue
-            author
-            order
+            publicationMonthYear: date(formatString: "MMMM YYYY")
             backgroundColor
             textColor
           }
