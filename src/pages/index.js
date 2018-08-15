f05f4ce7f6ddde5b267c56c441c7755a845d17c1
Willis Plummer <willisplummer@gmail.com>
2018-08-15 00:04:57 -0400

    dont show image if blog is open

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,16 @@ class IndexPage extends Component {
   }
 
   componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll, true);
+    // TODO: put this function somewhere reusable
+    const blogHash = `#etc`;
+    const showBlog =
+      typeof window !== 'undefined' && window.location.hash === blogHash;
+    if (showBlog) {
+      // eslint-disable-next-line
+      this.setState({ hasScrolled: true });
+    } else {
+      window.addEventListener('scroll', this.handleScroll, true);
+    }
   }
 
   componentWillUnmount() {
